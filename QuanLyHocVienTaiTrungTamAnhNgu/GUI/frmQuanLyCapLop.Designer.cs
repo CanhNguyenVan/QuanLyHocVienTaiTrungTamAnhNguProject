5044d4158d15b0b147857989c217f8b7de27a3f5
cuongnsm <cuongeuro226@outlook.com>
2015-06-24 10:57:54 +0700

    Xong hết form nghiệp vụ nhé!

---
+++ QuanLyHocVienTaiTrungTamAnhNgu/GUI/frmQuanLyCapLop.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace GUI
+{
+    partial class frmQuanLyCapLop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewQuanLyCapLop = new System.Windows.Forms.DataGridView();
+            this.MaCapLop = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenCapLop = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewQuanLyCapLop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewQuanLyCapLop
+            // 
+            this.dataGridViewQuanLyCapLop.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewQuanLyCapLop.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaCapLop,
+            this.TenCapLop});
+            this.dataGridViewQuanLyCapLop.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewQuanLyCapLop.Name = "dataGridViewQuanLyCapLop";
+            this.dataGridViewQuanLyCapLop.Size = new System.Drawing.Size(399, 266);
+            this.dataGridViewQuanLyCapLop.TabIndex = 0;
+            // 
+            // MaCapLop
+            // 
+            this.MaCapLop.HeaderText = "Mã Cấp Lớp";
+            this.MaCapLop.Name = "MaCapLop";
+            this.MaCapLop.ReadOnly = true;
+            // 
+            // TenCapLop
+            // 
+            this.TenCapLop.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TenCapLop.HeaderText = "Tên Cấp Lớp";
+            this.TenCapLop.Name = "TenCapLop";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hủy";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(254, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Xóa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(173, 295);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Sửa";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(92, 295);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Thêm";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // frmQuanLyCapLop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 333);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridViewQuanLyCapLop);
+            this.Name = "frmQuanLyCapLop";
+            this.Text = "Quản Lý Cấp Lớp";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewQuanLyCapLop)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewQuanLyCapLop;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaCapLop;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenCapLop;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ QuanLyHocVienTaiTrungTamAnhNgu/GUI/frmQuanLyDiem.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace GUI
+{
+    partial class frmQuanLyDiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewQuanLyDiem = new System.Windows.Forms.DataGridView();
+            this.TenHocVien = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.TenKyThi = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Diem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_Huy = new System.Windows.Forms.Button();
+            this.btn_Xóa = new System.Windows.Forms.Button();
+            this.btn_Sua = new System.Windows.Forms.Button();
+            this.btn_Them = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewQuanLyDiem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewQuanLyDiem
+            // 
+            this.dataGridViewQuanLyDiem.AllowUserToOrderColumns = true;
+            this.dataGridViewQuanLyDiem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewQuanLyDiem.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TenHocVien,
+            this.TenKyThi,
+            this.Diem});
+            this.dataGridViewQuanLyDiem.Location = new System.Drawing.Point(14, 12);
+            this.dataGridViewQuanLyDiem.Name = "dataGridViewQuanLyDiem";
+            this.dataGridViewQuanLyDiem.Size = new System.Drawing.Size(672, 379);
+            this.dataGridViewQuanLyDiem.TabIndex = 0;
+            // 
+            // TenHocVien
+            // 
+            this.TenHocVien.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TenHocVien.HeaderText = "Tên Học Viên";
+            this.TenHocVien.Name = "TenHocVien";
+            // 
+            // TenKyThi
+            // 
+            this.TenKyThi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TenKyThi.HeaderText = "Tên Kỳ Thi";
+            this.TenKyThi.Name = "TenKyThi";
+            // 
+            // Diem
+            // 
+            this.Diem.HeaderText = "Điểm";
+            this.Diem.Name = "Diem";
+            this.Diem.Width = 150;
+            // 
+            // btn_Huy
+            // 
+            this.btn_Huy.Location = new System.Drawing.Point(610, 398);
+            this.btn_Huy.Name = "btn_Huy";
+            this.btn_Huy.Size = new System.Drawing.Size(75, 23);
+            this.btn_Huy.TabIndex = 1;
+            this.btn_Huy.Text = "Hủy";
+            this.btn_Huy.UseVisualStyleBackColor = true;
+            // 
+            // btn_Xóa
+            // 
+            this.btn_Xóa.Location = new System.Drawing.Point(529, 398);
+            this.btn_Xóa.Name = "btn_Xóa";
+            this.btn_Xóa.Size = new System.Drawing.Size(75, 23);
+            this.btn_Xóa.TabIndex = 1;
+            this.btn_Xóa.Text = "Xóa";
+            this.btn_Xóa.UseVisualStyleBackColor = true;
+            // 
+            // btn_Sua
+            // 
+            this.btn_Sua.Location = new System.Drawing.Point(448, 398);
+            this.btn_Sua.Name = "btn_Sua";
+            this.btn_Sua.Size = new System.Drawing.Size(75, 23);
+            this.btn_Sua.TabIndex = 1;
+            this.btn_Sua.Text = "Sửa";
+            this.btn_Sua.UseVisualStyleBackColor = true;
+            // 
+            // btn_Them
+            // 
+            this.btn_Them.Location = new System.Drawing.Point(367, 398);
+            this.btn_Them.Name = "btn_Them";
+            this.btn_Them.Size = new System.Drawing.Size(75, 23);
+            this.btn_Them.TabIndex = 1;
+            this.btn_Them.Text = "Thêm";
+            this.btn_Them.UseVisualStyleBackColor = true;
+            // 
+            // frmQuanLyDiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 428);
+            this.Controls.Add(this.btn_Them);
+            this.Controls.Add(this.btn_Sua);
+            this.Controls.Add(this.btn_Xóa);
+            this.Controls.Add(this.btn_Huy);
+            this.Controls.Add(this.dataGridViewQuanLyDiem);
+            this.Name = "frmQuanLyDiem";
+            this.Text = "Quản Lý Điểm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewQuanLyDiem)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewQuanLyDiem;
+        private System.Windows.Forms.DataGridViewComboBoxColumn TenHocVien;
+        private System.Windows.Forms.DataGridViewComboBoxColumn TenKyThi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Diem;
+        private System.Windows.Forms.Button btn_Huy;
+        private System.Windows.Forms.Button btn_Xóa;
+        private System.Windows.Forms.Button btn_Sua;
+        private System.Windows.Forms.Button btn_Them;
+    }
+}
